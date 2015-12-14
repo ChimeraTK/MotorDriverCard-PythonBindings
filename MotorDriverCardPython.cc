d3fd30f35eceac78c4367106f22e553d954a8be5
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-12-14 13:04:44 +0000

    Moved to LinearStepperMotor And expanded interface
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MotorDriverCard/trunk@1954 5f601a5c-88b9-47f3-b851-818f6741d00d

--- MotorDriverCardPython.cc
+++ MotorDriverCardPython.cc
@@ -1,28 +1,53 @@
 #include <MotorDriverCard/StepperMotor.h>
+#include <MotorDriverCard/LinearStepperMotor.h>
 #include <boost/python.hpp>
 #include <boost/noncopyable.hpp>
 
 using namespace mtca4u;
 namespace bp = boost::python;
 
+const std::string returnStatusAsString(LinearStepperMotorStatusAndError self);
+const std::string returnErrorAsString(LinearStepperMotorStatusAndError self);
+
+std::string global = "Status";
 BOOST_PYTHON_MODULE(motor) {
   //  class_<mtca4u::StepperMotorUnitsConverter>("StepperMotorUnitsConverter")
   //    .def("stepsToUnits", &StepperMotorUnitsConverter::stepsToUnits)
   //    .def("unitsToSteps", &World::unitsToSteps)
   //    ;
 
-  bp::class_<StepperMotorStatusAndError>("StepperMotorStatusAndError");
+  bp::class_<LinearStepperMotorStatusAndError>("StatusAndError", bp::init<>())
+      .def("getStatus", &returnStatusAsString)
+      .def("getError", &returnErrorAsString);
 
-  bp::class_<StepperMotor, boost::noncopyable>(
-      "StepperMotor", bp::init<std::string const &, std::string const &,
-                               unsigned int, std::string>())
-      .def("moveToPosition", &StepperMotor::moveToPosition)
-      .def("setTargetPosition", &StepperMotor::setTargetPosition)
-      .def("getTargetPosition", &StepperMotor::setTargetPosition)
-      .def("getCurrentPosition", &StepperMotor::getCurrentPosition)
-      .def("setCurrentPositionAs", &StepperMotor::setCurrentPositionAs)
-      .def("start", &StepperMotor::start)
-      .def("stop", &StepperMotor::stop)
-      .def("setEnabled", &StepperMotor::setEnabled)
-      .def("getEnabled", &StepperMotor::getEnabled);
+  bp::class_<LinearStepperMotor, boost::noncopyable>(
+      "LinearStepperMotor", bp::init<std::string const &, std::string const &,
+                                     unsigned int, std::string>())
+      .def("emergencyStop", &LinearStepperMotor::emergencyStop)
+      .def("moveToPosition", &LinearStepperMotor::moveToPosition)
+      .def("setTargetPosition", &LinearStepperMotor::setTargetPosition)
+      .def("getTargetPosition", &LinearStepperMotor::setTargetPosition)
+      .def("getCurrentPosition", &LinearStepperMotor::getCurrentPosition)
+      .def("setCurrentPositionAs", &LinearStepperMotor::setCurrentPositionAs)
+      .def("start", &LinearStepperMotor::start)
+      .def("getAutostart", &LinearStepperMotor::getAutostart)
+      .def("setAutoStart", &LinearStepperMotor::setAutostart)
+      .def("stop", &LinearStepperMotor::stop)
+      .def("setEnabled", &LinearStepperMotor::setEnabled)
+      .def("getEnabled", &LinearStepperMotor::getEnabled)
+      .def("getNegativeEndSwitchPosition",
+           &LinearStepperMotor::getNegativeEndSwitchPosition)
+      .def("getPositiveEndSwitchPosition",
+           &LinearStepperMotor::getPositiveEndSwitchPosition)
+      .def("getStatusAndError", &LinearStepperMotor::getStatusAndError)
+      .def("setStepperMotorUnitsConverter",
+           &LinearStepperMotor::setStepperMotorUnitsConverter);
+}
+
+const std::string returnStatusAsString(LinearStepperMotorStatusAndError self) {
+  return self.status.asString();
+}
+
+const std::string returnErrorAsString(LinearStepperMotorStatusAndError self) {
+  return self.error.asString();
 }
